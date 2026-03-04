a45415698b55b2b0d5dcd5c4fd10cd071aca70a8
masdjedibelal-dotcom <masdjedibelal@gmail.com>
2026-03-04 22:47:32 +0100

    Delete supabase directory

--- supabase/migrations/20250301150000_messages_reply_to.sql
+++
@@ -1,3 +0,0 @@
--- Antworten auf Nachrichten
-ALTER TABLE messages ADD COLUMN IF NOT EXISTS reply_to_id uuid REFERENCES messages(id);
-CREATE INDEX IF NOT EXISTS idx_messages_reply_to_id ON messages(reply_to_id);
